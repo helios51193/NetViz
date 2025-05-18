409afbfdfdbfb1300e66b0398f2fed14e72c581f
Pranjal Rathore <pranjal51193@gmail.com>
2025-05-18 17:00:56 +0530

    conditional options show in navbar

--- networkvisualizer/src/app/navbar/navbar.component.ts
+++ networkvisualizer/src/app/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
 import { ModalSevice } from '../modal-sevice.service';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -10,10 +12,25 @@ import { ModalSevice } from '../modal-sevice.service';
 export class NavbarComponent {
 
   modalService = inject(ModalSevice);
+  private router = inject(Router);
+  private destroyRef = inject(DestroyRef);
+  isGraphView = signal<boolean>(false);
+
+  ngOnInit(){
+    const routerSubscription = this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe((event) => {
+      this.isGraphView.set(event.urlAfterRedirects.includes('/graph/'));
+    })
+
+    this.destroyRef.onDestroy(() => {
+      routerSubscription.unsubscribe();
+    });
+  }
+  
   openPreferencesModal() {
     this.modalService.triggerOpenPreferenceModal();
   }
   openCentralitiesModal(){
     this.modalService.triggerOpenCentrailtiesModal();
   }
+
 }
